1684517b25a7d5e01e00f1e60b35687cb6e28074
Bojan95 <jbojanowski11@gmail.com>
2023-06-05 01:02:04 +0200

    add signup page with form data

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from app import app
 
 from datetime import datetime
-from flask import render_template
+from flask import render_template, request, redirect
 
 @app.template_filter("clean_date")
 def clean_date(dt):
@@ -56,3 +56,14 @@ def jinja():
 @app.route("/about")
 def about():
     return render_template("public/about.html")
+
+@app.route("/sign-up", methods=["GET", "POST"])
+def sign_up():
+    
+    if request.method == "POST":
+        req = request.form
+        print(req)
+
+        return redirect(request.url)
+    
+    return render_template("public/sign_up.html")
