172537457217feaad04b2c822ec23c8b5068fac1
Bojan95 <jbojanowski11@gmail.com>
2023-06-04 11:40:55 +0200

    add looping and all variables in view (public)

--- app/views.py
+++ app/views.py
@@ -6,6 +6,41 @@ from flask import render_template
 def index():
     return render_template("public/index.html")
 
+@app.route("/jinja")
+def jinja():
+    my_name = "Kuba"
+    age = 27
+    langs = ["Python", "JavaScript", "Ruby", "Java", "C"]
+    friends = {
+        "Tom": 30,
+        "Amy": 45,
+        "Tony": 23,
+        "Clarissa": 18
+    }
+    colours = ("Red", "Green")
+    cool = True
+    class GitRemote:
+        def __init__(self, name, description, url):
+            self.name=name
+            self.description=description
+            self.url=url
+            
+        def pull(self):
+            return f"pulling repo {self.name}"
+        
+        def clone(self):
+            return f"cloning into {self.url}"
+        
+    my_remote = GitRemote("flask jinja", "temple design tutorial", "https://github.com/kuba/jinja.git")
+    
+    def repeat(x, qty):
+        return x*qty
+    
+    return render_template("public/jinja.html", my_name=my_name, age=age,
+                           langs=langs, friends=friends, colours=colours,
+                           cool=cool, GitRemote=GitRemote, repeat=repeat,
+                           my_remote=my_remote)
+
 @app.route("/about")
 def about():
     return render_template("public/about.html")
